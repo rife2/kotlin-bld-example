7c3253ecda872982162c2d02fe9938f744e89ffc
Erik C. Thauvin <erik@thauvin.net>
2025-12-31 04:02:41 -0800

    Bump JUnit to version 6.0.1

--- src/bld/java/com/example/ExampleBuild.java
+++ src/bld/java/com/example/ExampleBuild.java
@@ -33,7 +33,7 @@ public class ExampleBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES);
 
         final var kotlin = version(2, 2, 20);
-        final var junit = version(6, 0, 0);
+        final var junit = version(6, 0, 1);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin));
         scope(test)
