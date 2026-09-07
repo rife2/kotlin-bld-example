98edb8d84ce11366a5a5a06cb876013fd9d1aa2c
Erik C. Thauvin <erik@thauvin.net>
2026-09-07 08:22:29 -0700

    Bump Kotlin to version 2.4.20

--- src/bld/java/com/example/ExampleBuild.java
+++ src/bld/java/com/example/ExampleBuild.java
@@ -33,7 +33,7 @@ public class ExampleBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, CENTRAL_SNAPSHOTS, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
-        final var kotlin = version(2, 4, 10);
+        final var kotlin = version(2, 4, 20);
         final var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin));
